d1cafa468739a34d043b7ba1919ea98e42a3d6e9
Chen Xiongjie <chen.xcmail@gmail.com>
2022-06-24 16:18:52 +0200

    added: category of block types

--- src/dmeditor/MoreBlocks.tsx
+++ src/dmeditor/MoreBlocks.tsx
@@ -1,26 +1,37 @@
-import { AccountBalanceOutlined, AppBlockingOutlined, AppRegistrationOutlined, BrowseGallery, CalendarMonth, CodeOffOutlined, CodeOutlined, CollectionsOutlined, FacebookOutlined, FormatQuote, FormatQuoteOutlined, GolfCourseOutlined, GridOn, GridViewOutlined, HtmlOutlined, ImageOutlined, NetworkCellOutlined, PermMediaOutlined, TabOutlined, TextSnippetOutlined, TitleOutlined, Twitter, VideocamOutlined, WidgetsOutlined } from '@mui/icons-material';
-import { ImageList, Input } from '@mui/material';
+import { AccountBalanceOutlined, AppBlockingOutlined, AppRegistrationOutlined, BrowseGallery, CalendarMonth, CodeOffOutlined, CodeOutlined, CollectionsOutlined, ExpandCircleDownOutlined, FacebookOutlined, FormatQuote, FormatQuoteOutlined, GolfCourseOutlined, GridOn, GridViewOutlined, HtmlOutlined, ImageOutlined, MenuOpenOutlined, NetworkCellOutlined, PermMediaOutlined, TabOutlined, TextSnippetOutlined, TitleOutlined, Twitter, VideocamOutlined, ViewListOutlined, WidgetsOutlined } from '@mui/icons-material';
+import { Collapse, ImageList, Input } from '@mui/material';
 import { useState } from 'react';
 import './MoreBlocks.css';
 
 export const MoreBlocks = (props:{onSelect:any})=>{
+    const blockCategory = [
+        {identifier: 'common', text: 'Common'},
+        {identifier: 'util', text: 'Utility'},
+        {identifier: 'content_block', text: 'Content'},
+        {identifier: 'superoffice', text: 'SuperOffice'},
+        {identifier: 'social_network', text: 'Social Network'}        
+        ];
+
+    const iconStyle={fontSize: 26};
+    
     const blockTypes = [
-        {text:"Heading", type:'heading', icon: <TitleOutlined style={{fontSize: '28px'}} /> },
-        {text:"Paragraph", type:'p', icon: <TextSnippetOutlined style={{fontSize: '28px'}} /> },
-        {text:"Table", type:'table', icon: <GridOn style={{fontSize: '28px'}} /> }, 
-        {text:"Text Media", type:'text_media', icon: <PermMediaOutlined style={{fontSize: '28px'}} /> },
-        {text:"Full image", type:'full_image', icon: <ImageOutlined style={{fontSize: '28px'}} /> },    
-        {text:"Quote", type:'quote', icon: <FormatQuoteOutlined style={{fontSize: '28px'}} /> },
-        {text:"Code", type:'quote', icon: <CodeOutlined style={{fontSize: '28px'}} /> },    
-        {text:"Content blocks", type:'content_block', icon: <GridViewOutlined style={{fontSize: '28px'}} /> },
-        {text:"Carousel", type:'carousel', icon: <CollectionsOutlined style={{fontSize: '28px'}} /> },
-        {text:"Tab", type:'tab', icon: <TabOutlined style={{fontSize: '28px'}} /> },            
-        {text:"Calendar events", type:'calendar', icon: <CalendarMonth style={{fontSize: '28px'}} /> },    
-        {text:"Video", type:'video', icon: <VideocamOutlined style={{fontSize: '28px'}} /> },
-        {text:"Superoffice Form", type:'superoffice_form', icon: <AppRegistrationOutlined style={{fontSize: '28px'}} /> },          
-        {text:"Gallery", type:'gallery', icon: <CollectionsOutlined style={{fontSize: '28px'}} /> },     
-        {text:"Facebook", type:'facebook', icon: <FacebookOutlined style={{fontSize: '28px'}} /> },
-        {text:"Twitter", type:'twitter', icon: <Twitter style={{fontSize: '28px'}} /> },    
+        {text:"Heading", category:'common', type:'heading', icon: <TitleOutlined style={iconStyle} /> },
+        {text:"Paragraph", category:'common', type:'p', icon: <TextSnippetOutlined style={iconStyle} /> },
+        {text:"Table", category:'common', type:'table', icon: <GridOn style={iconStyle} /> }, 
+        {text:"Text media", category:'common', type:'text_media', icon: <PermMediaOutlined style={iconStyle} /> },
+        {text:"Full image", category:'common', type:'full_image', icon: <ImageOutlined style={iconStyle} /> },    
+        {text:"Video", category:'common', type:'video', icon: <VideocamOutlined style={iconStyle} /> },
+        {text:"Quote", category:'common', type:'quote', icon: <FormatQuoteOutlined style={iconStyle} /> },
+        {text:"Code", category:'common', type:'quote', icon: <CodeOutlined style={iconStyle} /> },    
+        {text:"Content blocks", category:'content_block', type:'content_block', icon: <GridViewOutlined style={iconStyle} /> },
+        {text:"Carousel", category:'content_block', type:'carousel', icon: <CollectionsOutlined style={iconStyle} /> },
+        {text:"Tab", category:'util', type:'tab', icon: <TabOutlined style={iconStyle} /> },            
+        {text:"Expandable list", category:'util', type:'expandable_list', icon: <ViewListOutlined style={iconStyle} /> },                    
+        {text:"Calendar events", category:'content_block', type:'calendar', icon: <CalendarMonth style={iconStyle} /> },    
+        {text:"Superoffice form", category:'superoffice',  type:'superoffice_form', icon: <AppRegistrationOutlined style={iconStyle} /> },          
+        {text:"Gallery", category:'content_block',  type:'gallery', icon: <CollectionsOutlined style={iconStyle} /> },     
+        {text:"Facebook page", category:'social_network',  type:'facebook', icon: <FacebookOutlined style={iconStyle} /> },
+        {text:"Twitter tweet",  category:'social_network', type:'twitter', icon: <Twitter style={iconStyle} /> },    
         ];
 
     const [list, setList] = useState(blockTypes);
@@ -35,15 +46,19 @@ export const MoreBlocks = (props:{onSelect:any})=>{
 
     return (<div style={{background: 'white'}}>
         <div style={{fontSize:'20px', padding: '10px 4px'}}>Please choose a block type</div>
-
         <div style={{background:'white'}}>
         <Input fullWidth placeholder='Type to search' onChange={search} autoFocus style={{padding: '10px'}} />
         </div>
-        {list.map((blockType, index)=><div key={index} className="moreblock" onClick={()=>props.onSelect(blockType.type)}>
-               <table style={{width:'100%'}}>
-                   <tr><td style={{width: '36px'}}>{blockType.icon}</td><td style={{textAlign:'left'}}>{blockType.text}</td></tr>                    
+        {blockCategory.map((category)=><div>
+            {list.filter(item=>item.category==category.identifier).map((blockType, index)=>
+                <>{index==0&&<div style={{padding: '5px 0px', color: '#4f4f4f', margin:'5px 10px' }}>{category.text}</div>}
+                <div key={index} className="moreblock" onClick={()=>props.onSelect(blockType.type)}>
+                <table style={{width:'100%'}}>
+                    <tr><td style={{width: '28px'}}>{blockType.icon}</td><td style={{textAlign:'left'}}>{blockType.text}</td></tr>                    
                 </table>
-            </div>)}
+            </div></>
+            )}
+        </div>)}
 
     </div>)
     
