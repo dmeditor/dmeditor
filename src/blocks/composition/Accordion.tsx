37f4869818669308567e7a43da372f40321ef289
wxx <393002848@qq.com>
2022-12-30 17:27:37 +0800

    added:BlockTab and Accordion

---
+++ src/blocks/composition/Accordion.tsx
@@ -0,0 +1,170 @@
+import { ViewListOutlined,DeleteOutline,AddCircleOutlineOutlined} from "@mui/icons-material";
+import "./BlockTab.css";
+import React, {useEffect ,useState,useRef} from 'react';
+import {BlockList} from '../../BlockList';
+import { ToolDefinition, ToolRenderProps } from "../../ToolDefinition";
+import { BlockProperty } from "../../BlockProperty"
+import { CommonSettings } from '../../CommonSettings';
+import {PropertyGroup,  PropertyItem,PropertyButton } from "../../utils/Property";
+import Accordion from 'react-bootstrap/Accordion';
+import { nanoid } from "nanoid";
+
+
+const BlockAccordion = (props:ToolRenderProps)=>{
+    const [commonSettings, setCommonSettings] = useState(props.data.common);
+    const [activeTabIndex, setActiveTabIndex] = useState(-1);
+    const [key, setKey] = useState(0);
+    const [activeIndex, setActiveIndex] = useState(0);
+    const [accordionList,setAccordionList] =  useState<Array<any>>(()=>{
+      if(props.data.children){
+        return props.data.children.map(item=>{
+          return {
+            ...item,
+            ...{contentEditable:false}
+          }
+        })
+      }else{
+        return []
+      }
+    });
+    const [isChange,setIsChange] = useState(false);
+    const expandableItemRef:any=useRef(null);
+    
+    const onChange = (item:any,data:any, index:number)=>{
+      let newAccordionList=[...accordionList]
+      newAccordionList[index]= item;
+      setAccordionList(newAccordionList);
+    }
+
+    const changeExpandableItemName = (e:any,index:any)=>{
+      let newAccordionList=[...accordionList]
+      newAccordionList[index].contentEditable=false;
+      const texts=e.target.innerText
+      newAccordionList[index].data=texts;
+      setActiveTabIndex(-1);
+      setKey(index)
+      setAccordionList([...newAccordionList])
+    }
+
+    const deletExpandableItem  = (index:any)=>{
+      let newAccordionList=[...accordionList]
+      newAccordionList.splice(index,1)
+      setActiveTabIndex(-1);
+      setAccordionList([...newAccordionList])
+      if( newAccordionList.length===0 ){
+        setKey(-1);
+      }else{
+        if(key==0){
+          setKey(0);
+        }else{
+          if(index<=key){
+            setKey(key-1);
+          }
+        }
+      }
+    }
+    
+    const addExpandableItem = ()=>{
+      let newAccordionList=[...accordionList]
+      let list={
+        type:'list',id:nanoid(),data:'', children:[
+          {"type":"text", id:nanoid(), "data":[
+            {type:"paragraph","children":[
+                {"text":"Default text"}
+            ],"common":{}, "setting":{}},
+          ]
+        }, 
+        ]
+      }
+      setAccordionList([...newAccordionList,list])
+    }
+   
+
+    useEffect(()=>{
+        props.onChange({...props.data, children:accordionList})
+        setIsChange(false);
+    }, [accordionList,isChange])
+
+  
+    return <>
+    {props.active&&<BlockProperty  blocktype="accordion" inBlock={true}>
+      {
+        accordionList.map((item,index)=>{
+          return (
+            <div className={`item ${ index === activeTabIndex? "active": "" }`}
+            key={item.id}>
+              <div ref={expandableItemRef} 
+                className="tabName" 
+                onDoubleClick={()=>{
+                    let newAccordionList=[...accordionList];
+                    newAccordionList[index].contentEditable=true;
+                    setAccordionList([...newAccordionList])
+                    setActiveTabIndex(index)
+                  }
+                } 
+                onBlur={(e)=>{changeExpandableItemName(e,index)}} 
+                suppressContentEditableWarning 
+                contentEditable={item.contentEditable}>
+                  {item.data}
+              </div>
+              <div><PropertyButton color="warning" title="Delete"  onClick={()=>{deletExpandableItem(index)}}><DeleteOutline /></PropertyButton></div>
+            </div>
+          )
+        })
+      }
+      <div className="item">
+        <div></div>
+        <div><PropertyButton color="warning" title="Add"  onClick={()=>{addExpandableItem()}}><AddCircleOutlineOutlined /></PropertyButton></div>
+      </div>
+      <div><CommonSettings commonSettings={commonSettings} settingList={['padding','backgroundColor','width']} onChange={(settings)=>{setCommonSettings(settings);setIsChange(true);}} /></div>
+    </BlockProperty>}
+    <div style={...commonSettings}>  
+      <Accordion className="expandableList" defaultActiveKey="0">
+        {
+          accordionList.map((item,index)=>{
+            return (
+              <Accordion.Item  key={item.id} eventKey={`'${index}'`}>
+                <Accordion.Header>{item.data}</Accordion.Header>
+                <Accordion.Body>
+                  <BlockList allowedType={['text', 'heading','image']} onChange={data=>onChange(item,data,index)} active={props.active&&activeIndex==1} list={item.children}  onActivate={()=>setActiveIndex(1)}/>
+                </Accordion.Body>
+              </Accordion.Item>
+            )
+          })
+        }
+      </Accordion>
+    </div>
+    </>
+}
+
+export const toolAccordion: ToolDefinition = {
+    type: "accordion",
+    name:"Accordion",
+    isComposited: true,
+    menu:  {category:'blocks',icon: <ViewListOutlined /> },
+    initData: ()=>{
+        return {
+          type:'accordion',
+          settings:{childrenHorizontal: false},
+          data:'',
+          common:{},
+          children:[ 
+            {type:'list',id:'1',data:'Title', children:[
+              {type:'heading', id:'1', data:'Title Heading', common:{color: '#ff0000'}, settings:{level: 2}},
+              {"type":"text", id:'2', "data":[
+                  {type:"paragraph","children":[
+                      {"text":"Default text"}
+                  ]},
+                 
+                ]
+              }, 
+              ],
+              "common":{}, "setting":{}
+            },
+            
+          ]
+        }
+    },
+    view: (props:{data:any})=><BlockAccordion data={props.data} active={false} onChange={()=>{}} inBlock={false} />,
+    render:BlockAccordion    
+}
---
+++ src/blocks/composition/BlockTab.tsx
@@ -0,0 +1,186 @@
+import { TabOutlined,DeleteOutline,AddCircleOutlineOutlined} from "@mui/icons-material";
+import "./BlockTab.css";
+import React, {useEffect ,useState,useRef} from 'react';
+import {BlockList} from '../../BlockList';
+import { ToolDefinition, ToolRenderProps } from "../../ToolDefinition";
+import { BlockProperty } from "../../BlockProperty"
+import { CommonSettings } from '../../CommonSettings';
+import {PropertyGroup,  PropertyItem,PropertyButton } from "../../utils/Property";
+import Tab from 'react-bootstrap/Tab';
+import Tabs from 'react-bootstrap/Tabs';
+import { nanoid } from "nanoid";
+
+
+const BlockTab = (props:ToolRenderProps)=>{
+    const [commonSettings, setCommonSettings] = useState(props.data.common);
+    const [activeTabIndex, setActiveTabIndex] = useState(-1);
+    const [key, setKey] = useState(0);
+    const [activeIndex, setActiveIndex] = useState(0);
+    const [tabList,setTabList] =  useState<Array<any>>(()=>{
+      if(props.data.children){
+        return props.data.children.map(item=>{
+          return {
+            ...item,
+            ...{contentEditable:false}
+          }
+        })
+      }else{
+        return []
+      }
+    });
+    const [isChange,setIsChange] = useState(false);
+    const tabRef:any=useRef(null);
+    
+    const onChange = (item:any,data:any, index:number)=>{
+        let newList = [...tabList];
+        newList[index]= item;
+        setTabList(newList);
+    }
+
+    const changeTabName = (e:any,index:any)=>{
+      let newTabList=[...tabList]
+      newTabList[index].contentEditable=false;
+      const texts=e.target.innerText
+      newTabList[index].data=texts;
+      setActiveTabIndex(-1);
+      setKey(index)
+      setTabList([...newTabList])
+    }
+
+    const deletTab = (index:any)=>{
+      let newTabList=[...tabList]
+      newTabList.splice(index,1)
+      setActiveTabIndex(-1);
+      setTabList([...newTabList])
+      if( newTabList.length===0 ){
+        setKey(-1);
+      }else{
+        if(key==0){
+          setKey(0);
+        }else{
+          if(index<=key){
+            setKey(key-1);
+          }
+
+          
+        }
+      }
+    }
+    
+    const addTab = ()=>{
+      let newTabList=[...tabList]
+      let list={
+        type:'list',id:nanoid(),data:'', children:[
+          {"type":"text", id:nanoid(), "data":[
+            {type:"paragraph","children":[
+                {"text":"Default text"}
+            ],"common":{}, "setting":{}},
+          ]
+        }, 
+        ]
+      }
+      setTabList([...newTabList,list])
+    }
+   
+
+    useEffect(()=>{
+        props.onChange({...props.data, children:tabList})
+        setIsChange(false);
+    }, [tabList,isChange])
+
+  
+    return <>
+    {props.active&&<BlockProperty  blocktype="tab" inBlock={true}>
+      {
+        tabList.map((item,index)=>{
+          return (
+            <div className={`item ${ index === activeTabIndex? "active": "" }`}
+            key={item.id}>
+              <div ref={tabRef} 
+                className="tabName" 
+                onDoubleClick={()=>{
+                    let newTabList=[...tabList];
+                    newTabList[index].contentEditable=true;
+                    setTabList([...newTabList])
+                    setActiveTabIndex(index)
+                  }
+                } 
+                onBlur={(e)=>{changeTabName(e,index)}} 
+                suppressContentEditableWarning 
+                contentEditable={item.contentEditable}>
+                  {item.data}
+              </div>
+              <div><PropertyButton color="warning" title="Delete"  onClick={()=>{deletTab(index)}}><DeleteOutline /></PropertyButton></div>
+            </div>
+          )
+        })
+      }
+      <div className="item">
+        <div></div>
+        <div><PropertyButton color="warning" title="Add"  onClick={()=>{addTab()}}><AddCircleOutlineOutlined /></PropertyButton></div>
+      </div>
+      <div><CommonSettings commonSettings={commonSettings} settingList={['padding','backgroundColor','width']} onChange={(settings)=>{setCommonSettings(settings);setIsChange(true);}} /></div>
+    </BlockProperty>}
+    <div style={...commonSettings}>  
+         <Tabs
+          className="blockTab"
+          activeKey={key}
+          onSelect={(k:any) => {setKey(k);setActiveIndex(0);setActiveTabIndex(-1);}}
+        >
+
+          {
+            tabList.map((item,index)=>{
+              return (
+                <Tab  key={item.id} eventKey={index} title={item.data}>
+                  <div>
+                    <BlockList allowedType={['text', 'heading','image']} onChange={data=>onChange(item,data,index)} active={props.active&&activeIndex==1} list={item.children}  onActivate={()=>setActiveIndex(1)}/>
+                  </div>
+                </Tab>
+              )
+            })
+          }
+        </Tabs>
+    </div>
+    </>
+}
+
+export const toolBlockTab: ToolDefinition = {
+    type: 'tab',
+    isComposited: true,
+    name:"Tab",
+    menu:  {category:'blocks',icon: <TabOutlined /> },
+    initData: ()=>{
+        return {
+          type:'tab',
+          settings:{childrenHorizontal: false},
+          data:'',
+          common:{},
+          children:[ 
+            {type:'list',id:'1',data:'Tab1', children:[
+              {type:'heading', id:'1', data:'Tab1 Title', common:{color: '#ff0000'}, settings:{level: 2}},
+              {"type":"text", id:'2', "data":[
+                  {type:"paragraph","children":[
+                      {"text":"Default text"}
+                  ]},
+                ]
+              }, 
+              ],
+              "common":{}, "setting":{}
+            },
+            {type:'list',id:'2',data:'Tab2', children:[
+              {type:'heading', id:'1', data:'Tab2 Title', common:{color: '#ff0000'}, settings:{level: 2}},
+              {"type":"text", id:'2', "data":[
+                  {type:"paragraph","children":[
+                      {"text":"Default text"}
+                  ]},
+                ]
+              }, 
+              ],
+              "common":{}, "setting":{}
+            }
+          ]
+        }
+    },
+    view: (props:{data:any})=><BlockTab data={props.data} active={false} onChange={()=>{}} inBlock={false} />,
+    render:BlockTab    
+}
