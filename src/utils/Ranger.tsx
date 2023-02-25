6056dd155b9c496939141e3f6ed7e395f239e9e5
wxx <393002848@qq.com>
2023-02-25 17:46:45 +0800

    updated:defaultValue and value

--- src/utils/Ranger.tsx
+++ src/utils/Ranger.tsx
@@ -3,11 +3,11 @@ import { Slider, Stack } from "@mui/material"
 export const Ranger=(props:{defaultValue?:number, value?:number, min:number, max: number, step?: number, onChange?:(v:number, event:any)=>void, onFinish?:(v:number)=>void})=>{
     return <Stack spacing={props.defaultValue} direction="row" sx={{ mb: 1 }} alignItems="center">
     <Slider aria-label="Temperature"
-  defaultValue={props.defaultValue}
+  defaultValue={props.defaultValue||0}
   valueLabelDisplay="auto"
    step={props.step}
   marks
-  value={props.value}
+  value={props.value?props.value:props.defaultValue||0}
   min={props.min}
   max={props.max}
   onChange={(e:any, v:number|Array<number>)=>{
