3b0b4a5bf6275a2184f2b4ce6ca1bd8a9d23ac97
Chen Xiongjie <chen.xcmail@gmail.com>
2023-04-21 16:04:20 +0200

    fixed: heading saving issue

--- src/blocks/Heading.tsx
+++ src/blocks/Heading.tsx
@@ -9,88 +9,58 @@ import TextField from '@mui/material/TextField';
 const Heading = (props:ToolRenderProps)=>{
     const [text,setText] = useState(props.data.data);
     const [level,setLevel] = useState(props.data.settings.level);
-    // const [align,setAlign] = useState(props.data.settings?.align||'left');
     const [id,setId] = useState(props.data.settings?.id||'');
     const [commonSettings,setCommonSettings] = useState(props.data.common?props.data.common:{});
-    const headRef:any=useRef(null);
-    const [isChange,setIsChange] = useState(false);
-    const alignList = ['left','center','right'];
-    const BlockButton = ({formats}:any) => {
-      let ele:any
-      if(formats ==='left'){
-        ele = <FormatAlignLeft />
-      }
-      if(formats ==='center'){
-        ele = <FormatAlignCenter />
-      }
-       if(formats ==='right'){
-        ele = <FormatAlignRight />
-      }
-      return ele
-    }
+    const defaultValue:any=useRef(props.data.data);    
     const changeText = (e?:any)=>{
-      const texts=headRef.current.innerText
+      const texts=e.target.innerText
       setText(texts);
-      setIsChange(true);
     }
 
-    const common = { onBlur:changeText,ref:headRef, contentEditable: props.active, style:{...commonSettings},}
+    const common = { onKeyUp:changeText, ...(id?{id:id}:{}), contentEditable: props.active, style:{...commonSettings},}
+
     const render = ()=>{
       switch(level){
         case 1:
-            return <h1 suppressContentEditableWarning {...common} id={id}>{text}</h1>
+            return <h1 suppressContentEditableWarning {...common}>{defaultValue.current}</h1>
         case 2:
-            return <h2  suppressContentEditableWarning {...common} id={id}>{text}</h2>
+            return <h2  suppressContentEditableWarning {...common}>{defaultValue.current}</h2>
         case 3:
-            return <h3 suppressContentEditableWarning {...common} id={id}>{text}</h3>
+            return <h3 suppressContentEditableWarning {...common}>{defaultValue.current}</h3>
         case 4:
-            return <h4 suppressContentEditableWarning {...common} id={id}>{text}</h4>
+            return <h4 suppressContentEditableWarning {...common}>{defaultValue.current}</h4>
         case 5:
-            return <h5 suppressContentEditableWarning {...common} id={id}>{text}</h5>
+            return <h5 suppressContentEditableWarning {...common}>{defaultValue.current}</h5>
         default:
-            return <h2 suppressContentEditableWarning {...common} id={id}>{text}</h2>
+            return <h2 suppressContentEditableWarning {...common}>{defaultValue.current}</h2>
       }
     }
 
     const autoCreateId = ()=>{
-      const texts=headRef.current.innerText
+      const texts=text
       let newId = texts.trim().replace(/\s/g,'-').replace(/[^\w\-]/g,'').toLowerCase()
-      console.log(newId);
       setId(newId)
     }
 
     useEffect(()=>{
-     if(isChange){
       let newData = {...props.data,data:text,settings:{level:level,id:id}, common: {...commonSettings}}
       props.onChange(newData);
-      setIsChange(false);
-     }
-    },[isChange])
+    },[level, text, id, commonSettings])
 
     return (
       <>
         {props.active&&<BlockProperty  blocktype="heading" inBlock={props.inBlock}>
           <PropertyItem label="Level">
-                <Ranger defaultValue={level} min={1} max={5} step={1} onChange={(v:any)=>{setLevel(v);setIsChange(true);}} />
-          </PropertyItem>   
-          {/* <PropertyItem label="Align">
-            {alignList.map((format:any,index:any)=>{           
-              return (
-                <PropertyButton title={format} key={format} onClick={()=>{setAlign(format);setIsChange(true);}}
-                  selected={align==format ? true : false}>
-                  <BlockButton formats={format} />
-                </PropertyButton>    
-                )             
-            })}
-          </PropertyItem> */}
+                <Ranger defaultValue={level} min={1} max={5} step={1} onChange={(v:any)=>{setLevel(v)}} />
+          </PropertyItem>            
           <PropertyItem label="Anchor">
-            <TextField sx={{width:'calc(100% - 37px)'}}  placeholder='Please enter ID'  value={id} size="small" hiddenLabel variant="outlined" onChange={(e)=>{setId(e.target.value);setIsChange(true);}} />
+            <TextField sx={{width:'calc(100% - 37px)'}}  placeholder='Please enter ID'  value={id} size="small" hiddenLabel variant="outlined" onChange={(e)=>{setId(e.target.value);}} />
             <PropertyButton title="Auto generate Id" onClick={()=>{autoCreateId()}}>
               <LoopOutlined/>
             </PropertyButton> 
           </PropertyItem>  
           {Util.renderCustomProperty(props.data)}
-        <div><CommonSettings commonSettings={commonSettings} onChange={(settings)=>{setCommonSettings(settings);setIsChange(true);}} /></div>
+        <div><CommonSettings commonSettings={commonSettings} onChange={(settings)=>{setCommonSettings(settings);}} /></div>
         </BlockProperty>}
         {render()}  
     </> 
@@ -107,7 +77,7 @@ const Heading = (props:ToolRenderProps)=>{
       type:'heading', 
       data:'heading',
       common:{width:'auto'},
-      settings:{level: 2,align:'left'},
+      settings:{level: 2},
    }
   },
   view: (props:{data:any})=><Heading inBlock={false} data={props.data} active={false} onChange={()=>{}} />,
