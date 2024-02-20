0fcf3f9a4cefbe8ded7e12c1f9ecc38788f34cd2
Chen Xiongjie <chen.xcmail@gmail.com>
2024-02-20 15:19:02 +0100

    fixed: input issue

--- src/core/setting-panel/property-setting/input/Input.tsx
+++ src/core/setting-panel/property-setting/input/Input.tsx
@@ -1,18 +1,20 @@
 import { TextField } from '@mui/material';
 import { useEditorStore } from 'Src/core/main/store';
 import { DME } from 'Src/core/types/dmeditor';
+import { useEffect, useState } from 'react';
 
 
 const Input = ( props: DME.SettingComponentProps) => {
     const {property, value} = props;
     const {updateSelectedBlockProps} = useEditorStore();
 
+
     const handleChange = (v: string) => {
         updateSelectedBlockProps(property, v);
-      };
+    };
 
   return (
-    <TextField size='small' defaultValue={value} onChange={(e)=>handleChange(e.target.value)} />
+    <TextField size='small' value={value} onChange={(e)=>handleChange(e.target.value)} />
   );
 };
 
