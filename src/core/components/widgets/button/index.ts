11d660439479cc434bd541bbbf7fd3e188992d7c
Chen Xiongjie <chen.xcmail@gmail.com>
2024-02-21 19:21:02 +0100

    added: size in button

--- src/core/components/widgets/button/index.ts
+++ src/core/components/widgets/button/index.ts
@@ -1,22 +1,53 @@
-import { registerWidget, registerWidgetStyle } from "..";
-import buttonWidget from "./definition";
-import { Button } from "./Button";
+import { registerWidget, registerWidgetStyle } from '..';
+import { Button } from './Button';
+import buttonWidget from './definition';
 
-const register = () =>{
-    registerWidget(buttonWidget, Button )
-    registerWidgetStyle('button',
-    {
-        identifier:'type',
-        display:'inline-block',
-        name: 'Type',
-        options: [
-            {identifier: 'primary',
-             name: 'Primary',
-             cssClasses: {root: 'bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded'},
-             cssStyle:''
-            }
-        ]
-    });
-}
+const register = () => {
+  registerWidget(buttonWidget, Button);
+  registerWidgetStyle('button', {
+    identifier: 'type',
+    display: 'inline-block',
+    name: 'Type',
+    options: [
+      {
+        identifier: 'primary',
+        name: 'Primary',
+        cssClasses: { root: 'bg-blue-500 hover:bg-blue-700 text-white py-1 px-4 rounded' },
+        cssStyle: '',
+      },
+      {
+        identifier: 'cancel',
+        name: 'Cancel',
+        cssClasses: { root: 'bg-gray-100 hover:bg-gray-500 hover:text-white py-1 px-4 rounded' },
+        cssStyle: '',
+      },
+    ],
+  });
+  registerWidgetStyle('button', {
+    identifier: 'size',
+    display: 'inline-block',
+    name: 'Size',
+    options: [
+      {
+        identifier: 'small',
+        name: 'Small',
+        cssClasses: { root: 'py-1 px-4' },
+        cssStyle: '',
+      },
+      {
+        identifier: 'medium',
+        name: 'Medium',
+        cssClasses: { root: 'py-2 px-4' },
+        cssStyle: '',
+      },
+      {
+        identifier: 'large',
+        name: 'Large',
+        cssClasses: { root: 'py-3 px-6' },
+        cssStyle: '',
+      },
+    ],
+  });
+};
 
-export default register ;
+export default register;
