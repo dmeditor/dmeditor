567130fb3941997307f12e143a76b5898b7571a2
Chen Xiongjie <chen.xcmail@gmail.com>
2024-02-21 10:31:10 +0100

    updated: always have theme option for widget - even if there is nothing in the theme (the switched theme may have)

--- src/core/setting-panel/style-settings/DropDown.tsx
+++ src/core/setting-panel/style-settings/DropDown.tsx
@@ -14,7 +14,7 @@ export const DropDown = (props: StyleSettingProps & { style: DME.WidgetStyle }) 
       >
         <MenuItem value="">None</MenuItem>
         {style.options.map((option) => (
-           (option.cssClasses!==undefined||option.cssStyle!=='')?<MenuItem key={option.identifier} value={option.identifier}>{option.name}</MenuItem>:<></>
+           <MenuItem key={option.identifier} value={option.identifier}>{option.name}</MenuItem>
         ))}
       </Select>
     );
