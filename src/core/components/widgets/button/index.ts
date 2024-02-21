83baa2a14a8bdf0249a489f48927b62fb6b2033e
Chen Xiongjie <chen.xcmail@gmail.com>
2024-02-21 19:29:32 +0100

    updated: button default size

--- src/core/components/widgets/button/index.ts
+++ src/core/components/widgets/button/index.ts
@@ -12,7 +12,7 @@ const register = () => {
       {
         identifier: 'primary',
         name: 'Primary',
-        cssClasses: { root: 'bg-blue-500 hover:bg-blue-700 text-white py-1 px-4 rounded' },
+        cssClasses: { root: 'bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded' },
         cssStyle: '',
       },
       {
