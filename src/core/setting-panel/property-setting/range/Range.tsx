ec036c41a2d863cfd8e41895e8805c6e4067e8b9
wing1003 <wing4win@hotmail.com>
2024-02-29 15:47:04 +0800

    fix: iframe props reactive slowly

--- src/core/setting-panel/property-setting/range/Range.tsx
+++ src/core/setting-panel/property-setting/range/Range.tsx
@@ -3,24 +3,30 @@ import * as React from 'react';
 import { useEditorStore } from 'Src/core/main/store';
 import { Ranger } from 'Src/core/utils/Ranger';
 
+const { useState, useEffect } = React;
 const Range = (props: {
   property: string;
   value: number;
   parameters: { min: number; max: number };
 }) => {
   const { property, parameters, value } = props;
+  const [currentValue, setCurrentValue] = useState(value);
   const { updateSelectedBlockProps } = useEditorStore();
 
   const handleChange = (value: number) => {
-    updateSelectedBlockProps(property, value);
+    setCurrentValue(value);
   };
+
+  useEffect(() => {
+    updateSelectedBlockProps(property, currentValue);
+  }, [currentValue]);
 
   return (
     <Ranger
-      defaultValue={value}
+      defaultValue={currentValue}
       min={parameters?.min || 1}
       max={parameters?.max || 5}
-      step={1}
+      step={10}
       onChange={handleChange}
     ></Ranger>
   );
