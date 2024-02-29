b9afc856d8c57991477fd399e5e85c779c8ad4b0
Chen Xiongjie <chen.xcmail@gmail.com>
2024-02-29 09:32:26 +0100

    continue

--- src/core/components/widgets/table/store.ts
+++ src/core/components/widgets/table/store.ts
@@ -2,13 +2,22 @@ import { create } from 'zustand';
 import { immer } from 'zustand/middleware/immer';
 
 export const useTableStore = create<
-  { activeCellIndex: [number, number] } & { setActiveCellIndex: (index: [number, number]) => void }
+  { activeCellIndex: [number, number]; width: number } & {
+    setActiveCellIndex: (index: [number, number]) => void;
+    setWidth: (v: number) => void;
+  }
 >()(
   immer((set) => ({
     activeCellIndex: [0, 0],
+    width: 100,
     setActiveCellIndex: (index: [number, number]) => {
       set((state) => {
         state.activeCellIndex = index;
+      });
+    },
+    setWidth: (v: number) => {
+      set((state) => {
+        state.width = v;
       });
     },
   })),
