9f6b142458798b608f72e91656389f2055c31aa7
Chen Xiongjie <chen.xcmail@gmail.com>
2024-03-03 18:36:32 +0100

    updated: default text

--- src/core/components/widgets/text/definition.ts
+++ src/core/components/widgets/text/definition.ts
@@ -18,36 +18,7 @@ const TextWidget: DME.Widget = {
           value: [
             {
               type: 'paragraph',
-              children: [
-                { text: 'This is editable ' },
-                { text: 'rich', bold: true },
-                { text: ' text, ' },
-                { text: 'much', italic: true },
-                { text: ' better than a ' },
-                { text: '<textarea>', code: true },
-                { text: '!' },
-              ],
-            },
-            {
-              type: 'paragraph',
-              children: [
-                {
-                  text: "Since it's rich text, you can do things like turn a selection of text ",
-                },
-                { text: 'bold', bold: true },
-                {
-                  text: ', or add a semantically rendered block quote in the middle of the page, like this:',
-                },
-              ],
-            },
-            {
-              type: 'block-quote',
-              children: [{ text: 'A wise quote.' }],
-            },
-            {
-              type: 'paragraph',
-              align: 'center',
-              children: [{ text: 'Try it out for yourself!' }],
+              children: [{ text: 'Sample text' }],
             },
           ],
         },
