f1909c8dea3d05cc75d07a44e3b6e1c405d7faa2
Chen Xiongjie <chen.xcmail@gmail.com>
2024-03-06 10:53:06 +0100

    added: theme support for DMEditorView

--- src/core/main/renderer/DMEditorView.tsx
+++ src/core/main/renderer/DMEditorView.tsx
@@ -1,12 +1,32 @@
+import { useMemo } from 'react';
+import { css } from '@emotion/css';
+
 import { BlockListRender } from './BlockListRender';
+import { getPageTheme } from 'Src/core/components/page';
 import { DME, DMEData } from 'Src/core/types/dmeditor';
 
 export interface DMEditorViewProps {
   data: DMEData.BlockList;
+  theme?: string;
 }
 
 const DMEditorView = (props: DMEditorViewProps) => {
-  return <BlockListRender blockData={props.data} path={[]} mode="view" />;
+  const { data, theme } = props;
+
+  const getThemeCss = () => {
+    const pageTheme = getPageTheme(theme || 'default');
+    if (pageTheme) {
+      return css(pageTheme.cssStyle);
+    } else {
+      return undefined;
+    }
+  };
+
+  return (
+    <div className={getThemeCss()}>
+      <BlockListRender blockData={data} path={[]} mode="view" />
+    </div>
+  );
 };
 
 export { DMEditorView };
