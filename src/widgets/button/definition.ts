bfc93a8ca65985f8784927b23f13229d6c3dd1be
Chen Xiongjie <chen.xcmail@gmail.com>
2024-05-29 21:58:32 +0200

    continue

--- src/widgets/button/definition.ts
+++ src/widgets/button/definition.ts
@@ -35,7 +35,7 @@ const buttonWidget: DME.Widget = {
       property: '.link',
       settingComponent: 'link',
     },
-    ...generalSettings.filter((item) => item.group !== 'block'),
+    ...generalSettings.filter((item) => item.group !== 'style_content'),
   ],
 };
 
