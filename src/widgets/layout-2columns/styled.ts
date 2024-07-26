390b89431f7b63c2fcfac4c1351baed97ace0b95
Chen Xiongjie <chen.xcmail@gmail.com>
2024-07-26 21:52:47 +0200

    updated: 2 columns in mobile show up & down

--- src/widgets/layout-2columns/styled.ts
+++ src/widgets/layout-2columns/styled.ts
@@ -4,4 +4,8 @@ export const SyledLayout = styled.div<{ columnWidth: number }>`
   display: grid;
   grid-template-columns: ${(props) => (props.columnWidth / 12) * 100}% ${(props) =>
       (1 - props.columnWidth / 12) * 100}%;
+
+  .dme-viewmode-mobile & {
+    display: block;
+  }
 `;
