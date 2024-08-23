f764060e772018502ae2921897f9577d48e4edba
Chen Xiongjie <chen.xcmail@gmail.com>
2024-08-23 22:51:20 +0200

    added: ssr error with more specific message

--- src/core/ssr/index.ts
+++ src/core/ssr/index.ts
@@ -18,16 +18,24 @@ function getWidgetServerSideLoad(widgetName: string) {
 
 const dmeServerSideLoad = async (data: DMEData.BlockList, serverParameters: any) => {
   let proms: Array<Promise<any>> = [];
+  let errorInfos: Array<string> = [];
   iterateBlockList(data, (blockItem: DMEData.Block) => {
     const blockType = blockItem.type;
     const func = getWidgetServerSideLoad(blockType);
     if (func) {
+      errorInfos.push('Type: ' + blockType + ', id: ' + blockItem.id);
       proms.push(func(blockItem, serverParameters));
     }
   });
 
   try {
-    await Promise.all(proms);
+    await Promise.all(
+      proms.map((p, index) =>
+        p.catch((e) => {
+          throw 'error on ' + errorInfos[index] + e;
+        }),
+      ),
+    );
     return data;
   } catch (error) {
     throw 'Error in DM Editor server load ' + error;
