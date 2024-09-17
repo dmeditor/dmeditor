1740c36497d62c034186fb169f4fdec8a0fd28a9
Chen Xiongjie <chen.xcmail@gmail.com>
2024-09-17 21:26:08 +0200

    added: chi-CN translation basically

--- src/core/i18n/index.ts
+++ src/core/i18n/index.ts
@@ -1,11 +1,13 @@
 import i18nCore from 'i18next';
 
+import chiCN from '../../locales/chi-CN.json';
 import engGB from '../../locales/eng-GB.json';
 import norNo from '../../locales/nor-NO.json';
 
 const resources = {
   'eng-GB': engGB,
   'nor-NO': norNo,
+  'chi-CN': chiCN,
 };
 
 const initLanguage = (language: string) => {
