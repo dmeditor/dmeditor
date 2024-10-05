beeacb8ef12ae8b1144aee309610a4a0cd85b5ba
Chen Xiongjie <chen.xcmail@gmail.com>
2024-10-05 22:26:25 +0200

    fixed: tabs nav style key issue

--- src/core/components/nav/Nav.tsx
+++ src/core/components/nav/Nav.tsx
@@ -3,7 +3,7 @@ import * as React from 'react';
 interface NavProps extends React.HTMLAttributes<HTMLElement> {}
 const Nav = (props: NavProps) => {
   return (
-    <div role="tablist" className="flex">
+    <div role="tablist" className={'flex ' + (props.className || '')}>
       {props.children}
     </div>
   );
