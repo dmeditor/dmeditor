73cdb7743c4b85ab30ea6a34b09c53eed56f9eca
Chen Xiongjie <chen.xcmail@gmail.com>
2024-10-05 19:53:12 +0200

    udpated: removed default button styles(put to separate)

--- src/widgets/button/index.ts
+++ src/widgets/button/index.ts
@@ -6,50 +6,6 @@ import Icon from './icon/icon.svg';
 const register = () => {
   registerWidget(buttonWidget, { render: Button });
   registerIcon({ name: 'button', component: Icon });
-  registerWidgetStyle('button', {
-    identifier: 'type',
-    display: 'inline-block',
-    name: 'Type',
-    options: [
-      {
-        identifier: 'primary',
-        name: 'Primary',
-        cssClasses: { button: 'bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded' },
-        cssStyle: '',
-      },
-      {
-        identifier: 'cancel',
-        name: 'Cancel',
-        cssClasses: { button: 'bg-gray-100 hover:bg-gray-500 hover:text-white py-1 px-4 rounded' },
-        cssStyle: '',
-      },
-    ],
-  });
-  registerWidgetStyle('button', {
-    identifier: 'size',
-    display: 'inline-block',
-    name: 'Size',
-    options: [
-      {
-        identifier: 'small',
-        name: 'Small',
-        cssClasses: { button: 'py-1 px-4' },
-        cssStyle: '',
-      },
-      {
-        identifier: 'medium',
-        name: 'Medium',
-        cssClasses: { button: 'py-2 px-4' },
-        cssStyle: '',
-      },
-      {
-        identifier: 'large',
-        name: 'Large',
-        cssClasses: { button: 'py-3 px-6' },
-        cssStyle: '',
-      },
-    ],
-  });
 };
 
 export default register;
