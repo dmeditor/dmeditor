dcc8f731280c993b23b17d3d8e12929d2053af9e
Chen Xiongjie <chen.xcmail@gmail.com>
2024-10-05 19:54:00 +0200

    updated: default grid middle

--- src/widgets/grid/styled.ts
+++ src/widgets/grid/styled.ts
@@ -2,6 +2,7 @@ import styled from '@emotion/styled';
 
 export const StyledGrid = styled.div<{ columns: number; gap: number }>`
   display: grid;
+  align-items: center;
   grid-template-columns: repeat(${(props) => props.columns}, 1fr);
   ${(props) => (props.gap ? 'grid-gap:' + props.gap + 'px;' : '')}
 `;
