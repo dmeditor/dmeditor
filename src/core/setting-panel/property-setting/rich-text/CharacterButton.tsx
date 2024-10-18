1abd6b0e1640a0c20db96331e80639c82faae5ea
Chen Xiongjie <chen.xcmail@gmail.com>
2024-10-18 19:59:24 +0200

    added: clickaway event for character button

--- src/core/setting-panel/property-setting/rich-text/CharacterButton.tsx
+++ src/core/setting-panel/property-setting/rich-text/CharacterButton.tsx
@@ -1,7 +1,17 @@
 import { useEffect, useRef, useState } from 'react';
 import { css } from '@emotion/css';
 import { EmojiEmotionsOutlined } from '@mui/icons-material';
-import { Box, Collapse, Dialog, DialogContent, DialogTitle, Paper, Popper } from '@mui/material';
+import {
+  Box,
+  ClickAwayListener,
+  Collapse,
+  Dialog,
+  DialogContent,
+  DialogTitle,
+  Fade,
+  Paper,
+  Popper,
+} from '@mui/material';
 import { dmeConfig } from 'dmeditor/core/config';
 import { Transforms } from 'slate';
 import { useSlate } from 'slate-react';
@@ -29,32 +39,40 @@ const CharacterButton = () => {
       <Button ref={buttonRef} title="Insert character" onClick={showPanel}>
         <EmojiEmotionsOutlined />
       </Button>
-      <Popper open={panelShown} anchorEl={buttonRef.current}>
-        <Paper elevation={2} sx={{ p: 1 }}>
-          <div
-            className={css`
-              display: grid;
-              grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr;
-            `}
-          >
-            {dmeConfig.editor.characters.map((item) => (
-              <div
-                className={css`
-                  cursor: pointer;
-                  padding: 4px;
-                  text-align: center;
-                  font-size: 20px;
-                  &:hover {
-                    background: #f0f0f0;
-                  }
-                `}
-                onClick={() => insert(item)}
-              >
-                {item}
-              </div>
-            ))}
-          </div>
-        </Paper>
+      <Popper open={panelShown} anchorEl={buttonRef.current} transition>
+        {({ TransitionProps }) => (
+          <Fade {...TransitionProps}>
+            <div>
+              <ClickAwayListener onClickAway={() => setPanelShown(false)}>
+                <Paper elevation={2} sx={{ p: 1 }}>
+                  <div
+                    className={css`
+                      display: grid;
+                      grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr;
+                    `}
+                  >
+                    {dmeConfig.editor.characters.map((item) => (
+                      <div
+                        className={css`
+                          cursor: pointer;
+                          padding: 4px;
+                          text-align: center;
+                          font-size: 20px;
+                          &:hover {
+                            background: #f0f0f0;
+                          }
+                        `}
+                        onClick={() => insert(item)}
+                      >
+                        {item}
+                      </div>
+                    ))}
+                  </div>
+                </Paper>
+              </ClickAwayListener>
+            </div>
+          </Fade>
+        )}
       </Popper>
     </>
   );
