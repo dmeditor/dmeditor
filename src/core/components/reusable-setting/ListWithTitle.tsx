463f402c780df04105c059ba54fd818e5d1529d8
Chen Xiongjie <chen.xcmail@gmail.com>
2024-10-24 16:09:42 +0200

    added: plus for accordion

--- src/core/components/reusable-setting/ListWithTitle.tsx
+++ src/core/components/reusable-setting/ListWithTitle.tsx
@@ -1,5 +1,12 @@
 import * as React from 'react';
-import { ArrowDownwardOutlined, ArrowUpwardOutlined, DeleteOutline } from '@mui/icons-material';
+import {
+  AddOutlined,
+  ArrowDownwardOutlined,
+  ArrowUpwardOutlined,
+  DeleteOutline,
+} from '@mui/icons-material';
+import { IconButton } from '@mui/material';
+import { nanoid } from 'nanoid';
 
 import { useEditorStore } from '../../..';
 import type { DME } from '../../types';
@@ -54,43 +61,85 @@ const ListWithTitle = (props: DME.SettingComponentProps) => {
       block.children.splice(index, 1);
     });
   };
-  return Array.isArray(value)
-    ? value.map((item, index) => (
-        <div className="flex justify-between">
-          <div
-            className="hover:border-orange-300 hover:border hover:rounded w-40 p-1"
-            onBlur={(e) => changeTitle(e, index)}
-            suppressContentEditableWarning
-            contentEditable={true}
-          >
-            {item?.meta.title}
-          </div>
-          <div className="btn-groups">
-            {index !== 0 && (
-              <PropertyButton
-                color="warning"
-                title="move up"
-                onClick={(e) => handleMoveUp(e, index)}
+
+  const addUnder = () => {
+    updateBlockByPath(blockPath, (_, block) => {
+      if (!block.children) {
+        return;
+      }
+      block.children.push({
+        meta: {
+          tabKey: nanoid(),
+          title: 'New',
+        },
+        children: [
+          {
+            type: 'text',
+            id: nanoid(),
+            data: {
+              value: [
+                {
+                  type: 'paragraph',
+                  children: [{ text: 'Sample text' }],
+                },
+              ],
+            },
+          },
+        ],
+      });
+    });
+  };
+
+  return (
+    <div>
+      {Array.isArray(value) && (
+        <div>
+          {value.map((item, index) => (
+            <div className="flex justify-between">
+              <div
+                className="hover:border-orange-300 hover:border hover:rounded w-40 p-1"
+                onBlur={(e) => changeTitle(e, index)}
+                suppressContentEditableWarning
+                contentEditable={true}
               >
-                <ArrowUpwardOutlined />
-              </PropertyButton>
-            )}
-            {index !== value.length - 1 && (
-              <PropertyButton
-                color="warning"
-                title="move down"
-                onClick={(e) => handleMoveDown(e, index)}
-              >
-                <ArrowDownwardOutlined />
-              </PropertyButton>
-            )}
-            <PropertyButton color="warning" title="Delete" onClick={(e) => handleDelete(e, index)}>
-              <DeleteOutline />
-            </PropertyButton>
-          </div>
+                {item?.meta.title}
+              </div>
+              <div className="btn-groups">
+                {index !== 0 && (
+                  <PropertyButton
+                    color="warning"
+                    title="move up"
+                    onClick={(e) => handleMoveUp(e, index)}
+                  >
+                    <ArrowUpwardOutlined />
+                  </PropertyButton>
+                )}
+                {index !== value.length - 1 && (
+                  <PropertyButton
+                    color="warning"
+                    title="move down"
+                    onClick={(e) => handleMoveDown(e, index)}
+                  >
+                    <ArrowDownwardOutlined />
+                  </PropertyButton>
+                )}
+                <PropertyButton
+                  color="warning"
+                  title="Delete"
+                  onClick={(e) => handleDelete(e, index)}
+                >
+                  <DeleteOutline />
+                </PropertyButton>
+              </div>
+            </div>
+          ))}
         </div>
-      ))
-    : null;
+      )}
+      <IconButton title="Add" onClick={() => addUnder()}>
+        <AddOutlined />
+      </IconButton>
+    </div>
+  );
 };
 
 export default ListWithTitle;
