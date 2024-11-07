954c59f350eba2eb8dbe11ac20347445a171562a
Chen Xiongjie <chen.xcmail@gmail.com>
2024-11-07 22:48:54 +0100

    updated: configured sample color palette

--- src/core/utils/PickColor.tsx
+++ src/core/utils/PickColor.tsx
@@ -7,40 +7,93 @@ import { SketchPicker, type ColorResult } from 'react-color';
 
 import { DME, useEditorStore } from '../..';
 
-// 8 columns
+// 10 columns
 const PRESET_COLORS = [
-  '#f44336',
-  '#e91e63',
-  '#9c27b0',
-  '#673ab7',
-  '#3f51b5',
-  '#2196f3',
-  '#03a9f4',
-  '#00bcd4',
-  '#009688',
-  '#4caf50',
-  '#8bc34a',
-  '#cddc39',
-  '#ffeb3b',
-  '#ffc107',
-  '#ff9800',
-  '#ff5722',
-  '#795548',
-  '#607d8b',
-  '#ffffff',
-  '#cccccc',
-  '#999999',
-  '#666666',
-  '#333333',
   '#000000',
+  '#434343',
+  '#666666',
+  '#999999',
+  '#b7b7b7',
+  '#cccccc',
+  '#d9d9d9',
+  '#efefef',
+  '#f3f3f3',
+  '#ffffff',
+  '#980000',
+  '#ff0000',
+  '#ff9900',
+  '#ffff00',
+  '#00ff00',
+  '#00ffff',
+  '#4a86e8',
+  '#0000ff',
+  '#9900ff',
+  '#ff00ff',
+  '#e6b8af',
+  '#f4cccc',
+  '#fce5cd',
+  '#fff2cc',
+  '#d9ead3',
+  '#d0e0e3',
+  '#c9daf8',
+  '#cfe2f3',
+  '#d9d2e9',
+  '#ead1dc',
+  '#dd7e6b',
+  '#ea9999',
+  '#f9cb9c',
+  '#ffe599',
+  '#b6d7a8',
+  '#a2c4c9',
+  '#a4c2f4',
+  '#9fc5e8',
+  '#b4a7d6',
+  '#d5a6bd',
+  '#cc4125',
+  '#e06666',
+  '#f6b26b',
+  '#ffd966',
+  '#93c47d',
+  '#76a5af',
+  '#6d9eeb',
+  '#6fa8dc',
+  '#8e7cc3',
+  '#c27ba0',
+  '#a61c00',
+  '#cc0000',
+  '#e69138',
+  '#f1c232',
+  '#6aa84f',
+  '#45818e',
+  '#3c78d8',
+  '#3d85c6',
+  '#674ea7',
+  '#a64d79',
+  '#85200c',
+  '#990000',
+  '#b45f06',
+  '#bf9000',
+  '#38761d',
+  '#134f5c',
+  '#1155cc',
+  '#0b5394',
+  '#351c75',
+  '#741b47',
+  '#5b0f00',
+  '#660000',
+  '#783f04',
+  '#7f6000',
+  '#274e13',
+  '#0c343d',
+  '#1c4587',
+  '#073763',
+  '#20124d',
+  '#4c1130',
 ];
 
 const colorList = css`
   display: grid;
-  grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr;
-  padding: 0;
-  margin: 0;
-  list-style: none;
+  grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr;
 `;
 
 const Divider = styled.div`
@@ -49,7 +102,7 @@ const Divider = styled.div`
   margin: 0 10px;
 `;
 
-const ColorItem = styled.li<{ selected?: boolean }>`
+const ColorItem = styled.span<{ selected?: boolean }>`
   transition: all 0.3s ease;
   &:hover {
     transform: scale(1.25);
@@ -58,7 +111,7 @@ const ColorItem = styled.li<{ selected?: boolean }>`
 
   position: relative;
   display: inline-block;
-  margin: 3px;
+  margin: 1.5px;
   width: 20px;
   height: 20px;
   border-radius: 50%;
@@ -103,8 +156,7 @@ const ColorPickerContainer = styled.div<{
 
 const ColorPickerWrapper = styled.div`
   position: relative;
-  padding: 16px;
-  width: 220px;
+  padding: 14px;
   border: 1px solid #ccc;
   border-radius: 4px;
 `;
@@ -129,7 +181,7 @@ const SimpleColor = (props: { value?: string; onSelected?: (color: string) => vo
   };
 
   return (
-    <ul className={colorList} style={{ marginTop: 8 }}>
+    <div className={colorList}>
       {presetColors.map((color, index) => {
         return (
           <ColorItem
@@ -140,7 +192,7 @@ const SimpleColor = (props: { value?: string; onSelected?: (color: string) => vo
           />
         );
       })}
-    </ul>
+    </div>
   );
 };
 
@@ -167,7 +219,7 @@ const AdvancedColor = (props: { value?: string; onChange?: (color: string) => vo
       styles={{
         default: {
           picker: {
-            width: 220,
+            width: 240,
             padding: 0,
             boxShadow: 'none',
           },
@@ -198,8 +250,8 @@ const RecentColor = (props: {
 
   return (
     <>
-      <p>Recent</p>
-      <ul className={colorList}>
+      <div style={{ marginTop: 10 }}>Recent</div>
+      <div className={colorList} style={{ marginTop: 10 }}>
         {recentColors.map((color, index) => {
           return (
             <ColorItem
@@ -211,7 +263,7 @@ const RecentColor = (props: {
             />
           );
         })}
-      </ul>
+      </div>
     </>
   );
 };
