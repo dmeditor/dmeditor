028faa80bdd6b45e4daa93ca21c836c9534b35c3
Chen Xiongjie <chen.xcmail@gmail.com>
2025-01-03 22:53:04 +0100

    fixed: can not delete when hero is selected

--- src/core/setting-panel/actions/DeleteBlock.tsx
+++ src/core/setting-panel/actions/DeleteBlock.tsx
@@ -8,7 +8,16 @@ export const DeleteBlock = (props: { blockPath: Array<number | string> }) => {
   const { removeByPath } = useEditorStore();
 
   const handleDelete = () => {
-    removeByPath(props.blockPath);
+    const length = props.blockPath.length;
+    const lastIndex = props.blockPath[length - 1];
+    if (typeof lastIndex === 'number') {
+      removeByPath(props.blockPath);
+    } else {
+      if (length >= 2) {
+        const parentPath = [...props.blockPath].slice(0, -1);
+        removeByPath(parentPath);
+      }
+    }
   };
 
   return (
