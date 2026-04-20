5cad8099e572941c685b94edc074746c44989006
Chen Xiongjie <chen.xcmail@gmail.com>
2026-04-20 11:12:25 +0200

    updated: translation

--- src/core/setting-panel/config/index.ts
+++ src/core/setting-panel/config/index.ts
@@ -1,3 +1,10 @@
-const defaultSettingTabs: { [key: string]: string } = { widget: 'Widget', style: 'Style' };
+import { i18n } from 'dmeditor/core/i18n';
+
+const defaultSettingTabs: { [key: string]: string } = {
+  widget: 'Widget',
+  get style() {
+    return i18n('Style');
+  },
+};
 
 export { defaultSettingTabs };
